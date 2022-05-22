778c3969088ba7ba9a64a47f108a72015c8cfff6
User <user@Users-MacBook-Pro.local>
2022-05-21 23:12:50 -0400

    Finishing up Footer

--- App.tsx
+++ App.tsx
@@ -2,30 +2,93 @@ import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
 import colors from './src/theme/colors';
 import fonts from './src/theme/fonts';
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
   return (
     <View style={styles.post}>
       {/*hEADER */}
+      <View style={styles.header}>
+        <Image
+          source={require('/Users/user/Desktop/ReactNative/InstagramClone/src/assets/pictures/2.jpeg')}
+          style={styles.userAvatar}
+        />
+
+        <Text style={styles.userName}>Jeuxdeau</Text>
+
+        <Entypo
+          name="dots-three-horizontal"
+          size={16}
+          style={styles.threeDots}
+        />
+      </View>
+
       {/*content */}
       <Image
-        source={{
-          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg ',
-        }}
+        source={require('/Users/user/Desktop/ReactNative/InstagramClone/src/assets/pictures/1.jpeg')}
         style={styles.image}
       />
 
       {/*footer */}
+      <View style={styles.footer}>
+        <View style={styles.iconContainer}>
+          <AntDesign
+            name={'hearto'}
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Ionicons
+            name="chatbubble-outline"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="send"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="bookmark"
+            size={24}
+            style={{marginLeft: 'auto'}}
+            color={colors.black}
+          />
+        </View>
+
+        <Text>
+          Liked by
+          <Text style={{fontWeight: fonts.weight.bold}}> Kristen </Text> and
+          <Text style={{fontWeight: fonts.weight.bold}}> 527 others </Text>
+        </Text>
+
+        {/* Post description */}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  post: {paddingTop: 50},
+  post: {},
+  header: {flexDirection: 'row', padding: 10, alignItems: 'center'},
+  userName: {fontWeight: fonts.weight.bold, color: colors.black},
+  threeDots: {},
+  iconContainer: {flexDirection: 'row', marginBottom: 5},
+  footer: {
+    padding: 10,
+  },
+
   image: {
     width: '100%',
-    height: '100%',
+    height: '70%',
   },
+  icon: {marginHorizontal: 5},
+  userAvatar: {width: 50, height: 50, borderRadius: 25, marginRight: 10},
 });
 
 export default App;
