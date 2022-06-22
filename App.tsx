562df0e594f1f1cea86a9342a7f661e0d7a3aea9
User <user@Users-MacBook-Pro.local>
2022-06-22 15:21:53 -0400

    finished 3.8 comments

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
 import HomeScreen from './src/screens/HomeScreen/HomeScreen';
+import CommentsScreen from './src/screens/CommentsScreen/CommentsScreen';
 
 const App = () => {
   return (
--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import colors from '../../theme/colors';
 import fonts from '../../theme/fonts';
@@ -7,21 +7,45 @@ import {IComment} from '../../types/models';
 
 interface ICommentProps {
   comment: IComment;
+  includeDetails: boolean;
 }
 
-const Comment = ({comment}: ICommentProps) => {
+const Comment = ({comment, includeDetails = false}: ICommentProps) => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const toggleLike = () => {
+    setIsLiked(v => !v);
+  };
+
   return (
     <View style={styles.comment}>
-      <Text style={styles.commentText}>
-        <Text style={styles.bold}> {comment.user.username} </Text>
-        {comment.comment}
-      </Text>
-      <AntDesign
-        name={'hearto'}
-        size={14}
-        style={styles.icon}
-        color={colors.black}
-      />
+      {includeDetails && (
+        <Image source={{uri: comment.user.image}} style={styles.avatar} />
+      )}
+
+      <View style={styles.middleColumn}>
+        <Text style={styles.commentText}>
+          <Text style={styles.bold}> {comment.user.username} </Text>
+          {comment.comment}
+        </Text>
+
+        {includeDetails && (
+          <View style={styles.footer}>
+            <Text style={styles.footerText}> 2d </Text>
+            <Text style={styles.footerText}> 28 likes </Text>
+            <Text style={styles.footerText}> Reply </Text>
+          </View>
+        )}
+      </View>
+
+      <Pressable onPress={toggleLike} hitSlop={5}>
+        <AntDesign
+          name={isLiked ? 'heart' : 'hearto'}
+          size={14}
+          style={styles.icon}
+          color={isLiked ? colors.accent : colors.black}
+        />
+      </Pressable>
     </View>
   );
 };
@@ -33,7 +57,23 @@ const styles = StyleSheet.create({
     fontWeight: fonts.weight.bold,
   },
 
-  commentText: {color: colors.black, lineHeight: 18, flex: 1},
+  avatar: {
+    width: 40,
+    aspectRatio: 1,
+    borderRadius: 25,
+    marginRight: 5,
+  },
+
+  footer: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+
+  footerText: { marginRight: 10 },
+
+  middleColumn: {flex: 1},
+
+  commentText: {color: colors.black, lineHeight: 18},
 
   comment: {flexDirection: 'row', alignItems: 'center'},
 });
